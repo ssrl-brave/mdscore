01c8c67f1de17c99b67ffc6d72feb295ae6f2b8c
Arup <50182847+arupmondal835@users.noreply.github.com>
2025-10-29 03:12:44 -0600

    Update rdkit_template.py

--- analysis/rdkit_template.py
+++ analysis/rdkit_template.py
@@ -4,11 +4,11 @@ sml=open('out.smi').readline().strip()
 mol_from_smiles = Chem.MolFromSmiles(sml)
 #mol_with_h = Chem.AddHs(mol_from_smiles)
 
-template_mol = Chem.MolFromPDBFile('Mpro-x2545_iso1_model_0_ligand.pdb', removeHs=False)
+template_mol = Chem.MolFromPDBFile('Mpro-x0040_iso1_model_0_ligand.pdb', removeHs=False)
 
 aligned_mol = AllChem.AssignBondOrdersFromTemplate( mol_from_smiles,template_mol)
 
 #aligned_mol_h=Chem.AddHs(aligned_mol)
 #Chem.MolToPDBFile(aligned_mol_h, "aligned_with_H.pdb")
-Chem.MolToPDBFile(aligned_mol, "aligned.pdb")
+Chem.MolToPDBFile(aligned_mol, "Mpro-x0040_iso1_model_0_ligand_aligned.pdb")
 
